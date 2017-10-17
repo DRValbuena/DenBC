de787c830929d239ac4f12d16a7f95a85cd741fa
Denice Ron Valbuena <denice.ron.valbuena@gmail.com>
2017-10-17 16:45:46 -0700

    add small sample of Year Make Model

--- assets/js/theme/home.js
+++ assets/js/theme/home.js
@@ -1,3 +1,27 @@
+import $ from 'jquery';
 import PageManager from './page-manager';
 
-export default class Home extends PageManager {}
+export default class Home extends PageManager {
+  loaded(next) {
+    $(".class1").hide();
+    $(".class2").hide();
+    $("." + $("#first").val()).show();
+
+    $("#first").change(function() {
+      $(".class1").hide();
+      $("." + this.value).show();
+      $("#second option:first-child").prop("selected", true);
+      $("#third option:first-child").prop("selected", true);
+    });
+
+    $("#second").change(function() {
+      $(".class2").hide();
+      $("." + this.value).show();
+      $("#third option:first-child").prop("selected", true);
+      $(".vehicleSearch").attr('href',"/"+this.value+"/"+$("#third").val());
+    });
+    $("#third").change(function() {
+      $(".vehicleSearch").attr('href',"/"+$("#first").val()+"/"+$("#second").val()+"/"+$("#third").val());
+    });
+  }
+}
